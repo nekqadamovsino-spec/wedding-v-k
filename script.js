1c4b9d73250f5db00abf0b1692b6d751baf638e0
nekqadamovsino-spec <nekqadamovsino@gmail.com>
2026-07-08 01:13:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const WEB_APP_URL = "https://script.google.com/macros/s/AKfycby76RvD5_bBrh-sse3DruKMMle5Nt4sPr5psJBt6MoCcDNck6c3MOaKKGJCIworT4Xvug/exec";
+const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbxvGNsM31C1bC750yPF9woX2s9brOa7cBu3jRHKQMf135TBodb-I9a0aKJqPT8RkwCKIQ/exec";
 
 const form = document.getElementById("rsvpForm");
 
 if (form) {
-  form.addEventListener("submit", async function (e) {
+  form.addEventListener("submit", function (e) {
     e.preventDefault();
 
     const data = {
@@ -12,26 +12,13 @@ if (form) {
       presence: form.presence.value
     };
 
-    try {
-      const response = await fetch(WEB_APP_URL, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify(data)
-      });
+    fetch(WEB_APP_URL, {
+      method: "POST",
+      mode: "no-cors",
+      body: JSON.stringify(data)
+    });
 
-      const result = await response.json();
-
-      if (result.result === "success") {
-        alert("Спасибо! Ваш ответ отправлен.");
-        form.reset();
-      } else {
-        alert("Ошибка отправки.");
-      }
-    } catch (err) {
-      alert("Ошибка соединения.");
-      console.error(err);
-    }
+    alert("Спасибо! Ваш ответ отправлен.");
+    form.reset();
   });
 }
